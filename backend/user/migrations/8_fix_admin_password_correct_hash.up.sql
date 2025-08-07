2ba83e98e1b2908ac70bfa41da0f8860ea2f967e
Astra <26215963+3tternp@users.noreply.github.com>
2025-08-08 02:40:46 +0545

    Create 8_fix_admin_password_correct_hash.up.sql

---
+++ backend/user/migrations/8_fix_admin_password_correct_hash.up.sql
@@ -0,0 +1,5 @@
+-- Update the default admin user with the correct SHA256 hash for password "Admin123!@#"
+-- The correct hash is: 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918
+UPDATE users 
+SET password_hash = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
+WHERE id = 'default-admin';
